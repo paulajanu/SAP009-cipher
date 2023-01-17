e75875d3fda75d6cb0421cdf9f712ae34192c59b
paulajanu <annapsjanuario@gmail.com>
2023-01-17 17:57:44 -0300

    segundo teste

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const botaoResultado = document.getElementById("botaoCodificar");
+
+botaoResultado.addEventListener("click", acaoDoBotao);
+
+function acaoDoBotao(){
+    console.log("função funcionando");
+}
