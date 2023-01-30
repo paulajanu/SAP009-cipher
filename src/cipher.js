5ee98dc02128e0eba1cc644b2bc976df3324b99b
paulajanu <annapsjanuario@gmail.com>
2023-01-30 14:08:09 -0300

    refatoração

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,7 @@
+const CODIGO_DE_ASC_DA_LETRA_A = "A".charCodeAt(0);
+const CODIGO_DE_ASC_DA_LETRA_Z = "Z".charCodeAt(0);
+const TAMANHO_DO_ALFABETO = 26;
+
 const cipher = {
   encode: function (chave, txt) {
     if (typeof txt !== "string" || typeof chave !== "number" || txt === "" || chave === 0) {
@@ -9,13 +13,12 @@ const cipher = {
     
     for (let i = 0; i < txt.length; i++) { 
       const posicao = txt.charCodeAt(i); 
-      
-      if (posicao >= 65 && posicao <= 90) { 
-        txtCodificado += String.fromCharCode(((posicao - 65 + chave) % 26) + 65);
-      } 
-      if (posicao >= 32 && posicao <= 64) { 
+
+      if (posicao >= CODIGO_DE_ASC_DA_LETRA_A && posicao <= CODIGO_DE_ASC_DA_LETRA_Z) { 
+        txtCodificado += String.fromCharCode(((posicao - CODIGO_DE_ASC_DA_LETRA_A + chave) % TAMANHO_DO_ALFABETO) + CODIGO_DE_ASC_DA_LETRA_A);
+      } else {
         txtCodificado += txt.charAt(i);
-      } 
+      }       
     }
     return txtCodificado;
   },
@@ -31,11 +34,10 @@ const cipher = {
     for (let i = 0; i < txt.length; i++) {
       const posicao = txt.charCodeAt(i);
       
-      if (posicao >= 65 && posicao <= 90) {
-        txtDescodificado += String.fromCharCode (((posicao + 65 - chave) % 26) + 65); 
-      } 
-      if (posicao >= 32 && posicao <= 64) {
-        txtDescodificado += txt.charAt(i);
+      if (posicao >= CODIGO_DE_ASC_DA_LETRA_A && posicao <= CODIGO_DE_ASC_DA_LETRA_Z) {
+        txtDescodificado += String.fromCharCode (((posicao + CODIGO_DE_ASC_DA_LETRA_A - chave) % TAMANHO_DO_ALFABETO) + CODIGO_DE_ASC_DA_LETRA_A); 
+      } else {
+        txtDescodificado += txt.charAt(i)
       }
     } 
     return txtDescodificado;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import cipher from './cipher.js';
 
-const botaoResultadoCod = document.getElementById("botaoCodificar"); 
-const botaoResultadoDescod = document.getElementById("botaoDescodificar"); 
-const campoTxtEntrada = document.getElementById("entradaTexto");
-const campoTxtSaida = document.getElementById("saidaTexto");  
-const deslocamento = document.getElementById("chaveSecreta"); 
+const botaoResultadoCod = document.getElementById("botao-codificar"); 
+const botaoResultadoDescod = document.getElementById("botao-descodificar"); 
+const campoTxtEntrada = document.getElementById("entrada-texto");
+const campoTxtSaida = document.getElementById("saida-texto");  
+const deslocamento = document.getElementById("chave-secreta"); 
 
 botaoResultadoCod.addEventListener("click", acaoBotaoCod);
 
